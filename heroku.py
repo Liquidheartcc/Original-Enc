047b944eb1790164e4620f701dcd3201327abc58
Hotmoon <aravind1555.av@gmail.com>
2024-07-27 16:11:44 +0530

    Update heroku.py

--- heroku.py
+++ heroku.py
@@ -1,157 +1,65 @@
-async def enleech(event, args: str, client, direct=False):
+async def en_download(event, args, client):
     """
-    Adds a link or torrent link to encoding queue:
-        Requires a reply to link or the link as argument
-        can also add consecutive items to queue by replying
-        to the first link and a number of how many links to add to queue
-    Accepts the following flags:
-        -f filter (only use if familiar with filter format)
-        -rm what_to_remove (keyword to remove from torrent file_name)
-        -tc caption_tag (tag caption type as…)
-        -tf file_tag (tag file_as)
-        -v number (tag according to version number)
-    Both flags override /filter & /v
+    Downloads the replied message: to a location (specified) locally
+    Available arguments:
+      End the args with '/' to specify the folder in which to download and let the bot use its filename
+      or:
+        --dir DIR (Must be in double quotes.)
+        --home (To download to current working directory.)
+      --cap (To use download with caption instead of filename.)
+      if no other arg is given after dir, bot automatically downloads to given dir with default filename instead.
 
-    :: filter format-
-        what_to_remove\\ntag_file_as\\ntag_caption_as
-    ::
+      *path specified directly will be downloaded as a subdir to download folder
     """
-    chat_id = event.chat_id
-    user_id = event.sender_id
-    if not (user_is_allowed(user_id) or direct):
-        return
-    cust_fil = cust_v = str()
-    mode = "None"
-    o_args = None
-    queue = get_queue()
-    invalid_msg = "`Invalid torrent/direct link`"
-    no_uri_msg = (
-        "`uhm you need to reply to or send command alongside a uri/direct link`"
-    )
-    no_dl_spt_msg = "`File to download is…\neither not a video\nor is a batch torrent which is currently not supported.`"
-    ukn_err_msg = "`An unknown error occurred, might an internal issue with aria2.\nCheck logs for more info`"
-    if args:
-        o_args = args
-        flag, args = get_args(
-            "-f", "-rm", "-tc", "-tf", "-v", to_parse=args, get_unknown=True
-        )
-        if flag.rm or flag.tc or flag.tf:
-            cust_fil = flag.rm or "disabled__"
-            cust_fil += str().join(
-                f"\n{x}" if x else "\nauto" for x in [flag.tf, flag.tc]
-            )
-        else:
-            cust_fil = str_esc(flag.f)
-        cust_v = flag.v
+    if not user_is_owner(event.sender_id):
+        return await event.delete()
+    if not event.is_reply:
+        return await event.reply("`Reply to a file or link to download it`")
     try:
-        if event.is_reply:
-            rep_event = await event.get_reply_message()
-            if rep_event.file:
-                await event.reply("**Warning:** `Use /add for files instead.`")
-                return await addqueue(event, o_args, client)
-            if args:
-                if not args.isdigit():
-                    return await event.reply(
-                        f"**Yeah No.**\n`Error: expected a number but received '{args}'.`"
-                    )
-                args = int(args)
-                async with queue_lock:
-                    for _none, _id in zip(
-                        range(args), itertools.count(start=rep_event.id)
-                    ):
-                        event2 = await client.get_messages(event.chat_id, _id)
-                        if event2.empty:
-                            await event.reply(
-                                f"Resend uri links and try replying the first with /l or /leech again"
-                            )
-                            return await rm_pause(dl_pause, 5)
-                        uri = event2.text
-                        if not uri:
-                            return await event.reply(no_uri_msg)
-                        if not (is_url(uri) or is_magnet(uri)):
-                            await event2.reply(invalid_msg)
-                            return await rm_pause(dl_pause, 5)
-                        file = await get_leech_name(uri)
-                        if file.error:
-                            await event2.reply(f"`{file.error}`", quote=True)
-                            await asyncio.sleep(10)
-                            continue
-                        if not is_video_file(file.name):
-                            await event2.reply(no_dl_spt_msg, quote=True)
-                            await asyncio.sleep(5)
-                            continue
-                        already_in_queue = False
-                        for item in queue.values():
-                            if file.name in item:
-                                await event2.reply(
-                                    "**THIS LINK HAS ALREADY BEEN ADDED TO QUEUE**",
-                                    quote=True,
-                                )
-                                await asyncio.sleep(5)
-                                already_in_queue = True
-                                break
-                        if already_in_queue:
-                            continue
-                        if not bot_is_paused():
-                            pause(status=dl_pause)
-
-                        queue.update(
-                            {
-                                (chat_id, event2.id): [
-                                    file.name,
-                                    (user_id, event2),
-                                    (
-                                        cust_v or get_v(),
-                                        cust_fil or get_f(),
-                                        ("aria2", mode),
-                                    ),
-                                ]
-                            }
-                        )
-                        await save2db()
-
-                        msg = await event2.reply(
-                            f"**Added to queue ⏰, POS:** `{len(queue)-1}`\n`Please Wait , Encode will start soon`",
-                            quote=True,
-                        )
-                        await asyncio.sleep(5)
-                    if len(queue) > 1:
-                        asyncio.create_task(listqueue(msg, None, event.client, False))
-                    return await rm_pause(dl_pause)
+        _dir = None
+        loc = None
+        link = None
+        rep_event = await event.get_reply_message()
+        message = await client.get_messages(event.chat_id, int(rep_event.id))
+        if message.text and not (is_url(message.text) or is_magnet(message.text)):
+            return await message.reply("`Not a valid link`")
+        e = await message.reply(f"{enmoji()} `Downloading…`", quote=True)
+        if args is not None:
+            arg, args = get_args(
+                ["--home", "store_true"],
+                ["--cap", "store_true"],
+                "--dir",
+                to_parse=args,
+                get_unknown=True,
+            )
+            if args.endswith("/"):
+                _dir = args
             else:
-                uri = rep_event.text
-                event = rep_event
-        else:
-            uri = args
-
-        if not uri:
-            return await event.reply(no_uri_msg)
-        if not (is_url(uri) or is_magnet(uri)):
-            return await event.reply(invalid_msg)
-        file = await get_leech_name(uri)
-        if file.error:
-            return await event.reply(f"`{file.error}`")
-        if not is_url(uri):
-            return await event.reply(no_dl_spt_msg)
-        
-        async with queue_lock:
-            queue.update(
-                {
-                    (chat_id, event.id): [
-                        file.name,
-                        (user_id, None),
-                        (cust_v or get_v(), cust_fil or get_f(), ("aria2", mode)),
-                    ]
-                }
+                loc = args
+            if arg.home:
+                _dir = home_dir
+            elif arg.dir:
+                _dir = arg.dir
+            if arg.cap and not message.text:
+                loc = message.caption
+        link = message.text if message.text else link
+        if not loc:
+            loc = rep_event.file.name if not link else link
+        _dir = "downloads/" if not _dir else _dir
+        _dir += str() if _dir.endswith("/") else "/"
+        await try_delete(event)
+        d_id = f"{e.chat.id}:{e.id}"
+        download = downloader(_id=d_id, uri=link, folder=_dir)
+        await download.start(loc, 0, message, e)
+        if download.is_cancelled or download.download_error:
+            return await report_failed_download(
+                download, e, download.file_name, event.sender_id
             )
-            await save2db()
-        if len(queue) > 1 or bot_is_paused():
-            msg = await event.reply(
-                f"**Added To Queue ⏰, POS:** `{len(queue)-1}`\n`Please Wait , Encode will start soon`"
-            )
-            if len(queue) > 1:
-                return asyncio.create_task(listqueue(msg, None, event.client, False))
-    except Exception as e:
+        f_loc = _dir + loc if not link else _dir + download.file_name
+        new_file_name = "video.mp4"
+        new_f_loc = os.path.join(os.path.dirname(f_loc), new_file_name)
+        os.rename(f_loc, new_f_loc)
+        f_loc = new_f_loc
+        await e.edit(f"__Saved to__ `{f_loc}` __successfully!__")
+    except Exception:
         await logger(Exception)
-        await rm_pause(dl_pause)
-        return await event.reply(f"An error Occurred:\n - {e}")
